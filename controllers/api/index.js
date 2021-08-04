0cec2d6d70b34dfcfefc2f2dc671115e7b6e687a
Zach Maurer <zmaurer88@yahoo.com>
2021-08-04 10:07:52 -0700

    updated routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const piggyRoutes = require('./piggyRoutes');
+const farmerRoutes = require('./farmerRoutes');
 
+router.use('/users', userRoutes);
 router.use('/piggy', piggyRoutes);
-router.use('/piggy', piggyRoutes);
+router.use('/farmers', farmerRoutes);
 
 module.exports = router;
